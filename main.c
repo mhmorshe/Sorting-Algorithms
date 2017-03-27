5618b0303bc746b600467415b79900078543149c
mhmorshe <mhmorshe@ieng6-203.ucsd.edu>
2017-03-26 17:59:16 -0700

    finished bubble sort

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include "bubble_sort.c"
 void main()
 {
-  float unsorted[3] = {1.1, 1.2, 4.5};
-  bubble_sort(unsorted);
-  printf("This is a test\n");
+  float unsorted[9] = {1.1, 1.2, 4.5, 5.1, 3.2, 1.0, 0 , 54, 12.7};
+  bubble_sort(unsorted, 9);
+ 
 }
